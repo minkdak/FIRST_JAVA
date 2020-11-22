bed1152349b6921848b7e0de73c6e9f6c4eaad2b
Kim MinSeop <kmsp91@gmail.com>
2020-11-23 00:34:47 +0900

    Revert "dd"
    This reverts commit 476e4b56ca3fdec1f034468e932e41e208a78706.

--- Chapter8/src/apstractex/NoteBook.java
+++
@@ -1,9 +0,0 @@
-package apstractex;
-
-public abstract class NoteBook extends Computer{
-	public void typing() {
-		// TODO Auto-generated method stub
-		System.out.println("NoteBook typing");
-	}
-	
-}
